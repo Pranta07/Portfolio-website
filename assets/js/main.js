a0067b1c0bcc11ea1c6dfd8ae5d4ae20795afad4
Pranta07 <palpranta@gmail.com>
2021-07-24 23:45:16 +0600

    qualifications-added

--- assets/js/main.js
+++ assets/js/main.js
@@ -50,6 +50,25 @@ skillsHeader.forEach(e => e.addEventListener('click', toogleskills));
 
 
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+    tabcontents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.target)
+
+        tabcontents.forEach(tabContent => {
+            tabContent.classList.remove('qualification_active')
+        })
+        target.classList.add('qualification_active')
+
+        tabs.forEach(tab => {
+            tab.classList.remove('qualification_active')
+        })
+        tab.classList.add('qualification_active')
+    })
+})
+
 
 /*==================== SERVICES MODAL ====================*/
 
