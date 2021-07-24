1389f86f8cb66cd153d53ca7fd3f124943112638
Pranta07 <palpranta@gmail.com>
2021-07-25 03:45:28 +0600

    styles-changed

--- assets/js/main.js
+++ assets/js/main.js
@@ -76,5 +76,3 @@ function scrollHeader() {
     if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
-
-/*==================== DARK LIGHT THEME ====================*/
