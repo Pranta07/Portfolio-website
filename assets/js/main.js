583738769a44edef13b38bcabc3045e7a83b28d1
Pranta07 <palpranta@gmail.com>
2021-07-25 01:42:24 +0600

    header_box-shadow added

--- assets/js/main.js
+++ assets/js/main.js
@@ -69,17 +69,12 @@ tabs.forEach(tab => {
     })
 })
 
-
-/*==================== SERVICES MODAL ====================*/
-
-/*==================== PORTFOLIO SWIPER  ====================*/
-
-/*==================== TESTIMONIAL ====================*/
-
-/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-
 /*==================== CHANGE BACKGROUND HEADER ====================*/
-
-/*==================== SHOW SCROLL UP ====================*/
+function scrollHeader() {
+    const nav = document.getElementById('header')
+    // When the scroll is greater than 200 viewport height, add the scroll-header class to the header tag
+    if (this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== DARK LIGHT THEME ====================*/
